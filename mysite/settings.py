a602805786887fff21cee551c7ddafca97c45a8d
Can <can.wrob@gmail.com>
2022-09-29 23:08:08 +0200

    pt3 before Removing hardcoded URLs in templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from re import T
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-857p=83d5egxply4)@(-3jmxux&ika=2i&y!2!r7tocdmyvfo1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
